729b94d58f257de849424505517cbf17708e3ea0
rutheyd <r8507603@gmail.com>
2025-03-31 01:11:51 +0300

    footer

--- AuthClient/react/src/components/footer.tsx
+++ AuthClient/react/src/components/footer.tsx
@@ -1,49 +1,132 @@
-import { Box, Typography, IconButton } from '@mui/material';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
+// import React from 'react';
+// import 'font-awesome/css/font-awesome.min.css'; // הוספת קובץ ה-CSS של FontAwesome
+
+// const Footer = () => {
+//   return (
+//     <footer style={footerStyle}>
+//       <div style={footerContentStyle}>
+//         <p>&copy; 2025 AI BATTLE. All Rights Reserved.</p>
+//         <div style={socialLinksStyle}>
+//           <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+//             <i className="fa fa-facebook" style={iconStyle}></i>
+//           </a>
+//           <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+//             <i className="fa fa-youtube" style={iconStyle}></i>
+//           </a>
+//           <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+//             <i className="fa fa-twitter" style={iconStyle}></i>
+//           </a>
+//           <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+//             <i className="fa fa-instagram" style={iconStyle}></i>
+//           </a>
+//         </div>
+//         {/* חתימת הזכויות */}
+//         <p style={footerSignatureStyle}>
+//           &copy; 2025 AI-BATTLE. כל הזכויות שמורות.
+//         </p>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// const footerStyle: React.CSSProperties = {
+//   backgroundColor: '#333',
+//   color: 'white',
+//   padding: '20px 0',
+//   textAlign: 'center',
+//   position: 'relative',
+//   bottom: '0',
+//   width: '100%',
+// };
+
+// const footerContentStyle = {
+//   display: 'flex',
+//   flexDirection: 'column',
+//   justifyContent: 'center',
+//   alignItems: 'center',
+// };
+
+// const socialLinksStyle = {
+//   marginTop: '10px',
+// };
+
+// const iconStyle = {
+//   fontSize: '30px',
+//   margin: '0 15px',
+//   color: 'white',
+// };
+
+// const footerSignatureStyle: React.CSSProperties = {
+//   marginTop: '15px',
+//   fontSize: '14px',
+//   fontStyle: 'italic',
+//   color: 'white',
+// };
+
+// export default Footer;
+import React from 'react';
+import 'font-awesome/css/font-awesome.min.css'; // הוספת קובץ ה-CSS של FontAwesome
 
 const Footer = () => {
-    return (
-        <footer style={{
-            textAlign: 'center',
-            backgroundColor: 'white',
-            color: 'black', // צבע טקסט שחור כדי שיהיה קריא על רקע לבן
-            padding: '20px 20px',
-            position: 'fixed', // דבק את הפוטר לתחתית
-            bottom: 0,
-            left: 0,
-            right: 0,
-            fontFamily: '"Plus Jakarta Sans", Arial, Helvetica, sans-serif',
-        }}>
-            <Typography variant="body1">
-                © {new Date().getFullYear()} Pick A Pic. All rights reserved.
-            </Typography>
-            <Box sx={{ marginTop: 1 }}>
-                <IconButton 
-                    color="inherit" 
-                    href="https://www.youtube.com" 
-                    target="_blank"
-                >
-                    <YouTubeIcon />
-                </IconButton>
-                <IconButton 
-                    color="inherit" 
-                    href="https://www.facebook.com" 
-                    target="_blank"
-                >
-                    <FacebookIcon />
-                </IconButton>
-                <IconButton 
-                    color="inherit" 
-                    href="https://www.twitter.com" 
-                    target="_blank"
-                >
-                    <TwitterIcon />
-                </IconButton>
-            </Box>
-        </footer>
-    );
+  return (
+    <footer style={footerStyle}>
+      <div style={footerContentStyle}>
+        <p>&copy; 2025 AI BATTLE. All Rights Reserved.</p>
+        <div style={socialLinksStyle}>
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-facebook" style={iconStyle}></i>
+          </a>
+          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-youtube" style={iconStyle}></i>
+          </a>
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-twitter" style={iconStyle}></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-instagram" style={iconStyle}></i>
+          </a>
+        </div>
+        {/* חתימת הזכויות */}
+        <p style={footerSignatureStyle}>
+          &copy; 2025 AI-BATTLE. כל הזכויות שמורות.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+const footerStyle: React.CSSProperties = {
+  backgroundColor: '#333',
+  color: 'white',
+  padding: '20px 0',
+  textAlign: 'center',
+  position: 'relative',
+  bottom: '0',
+  width: '100%',
+};
+
+const footerContentStyle: React.CSSProperties = {
+  display: 'flex' as 'flex', // תוספת 'as flex'
+  flexDirection: 'column' as 'column', // תוספת 'as column'
+  justifyContent: 'center' as 'center', // תוספת 'as center'
+  alignItems: 'center' as 'center', // תוספת 'as center'
+};
+
+const socialLinksStyle: React.CSSProperties = {
+  marginTop: '10px',
+};
+
+const iconStyle: React.CSSProperties = {
+  fontSize: '30px',
+  margin: '0 15px',
+  color: 'white',
+};
+
+const footerSignatureStyle: React.CSSProperties = {
+  marginTop: '15px',
+  fontSize: '14px',
+  fontStyle: 'italic',
+  color: 'white',
 };
 
 export default Footer;
